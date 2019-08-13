e10fc69b1f0fd491d4d526727a1e2b13e0f954ac
Monica Lisa Reyes <mlisareyes@gmail.com>
2019-08-13 03:01:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  arr = [1, 2, 3, 4]
-  arr.push(5)
+  array << element
 end
   
 def add_element_to_start_of_array(array, element)
